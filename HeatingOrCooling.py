f884b9fef50f26d5f1b514bb91662667debad1e7
Luke De Guelle <33704864+AmazedCape518@users.noreply.github.com>
2019-07-29 18:39:00 -0500

    Heating or Cooling
    A calculator for determining the total days for heating or cooling.

---
+++ HeatingOrCooling.py
@@ -0,0 +1,23 @@
+# Chap8ex11
+# Luke De Guelle
+# A program that determines the total days for heating or cooling
+
+def main():
+	heating = 0
+	cooling = 0
+	day = eval(input("Enter the average temperature: "))
+	if (day < 60)
+		heating = heating + 1
+	else 
+		cooling = cooling + 1
+	choice = eval(input("Is there another day? (y/n)"))
+	while(choice = y)
+		day = eval(input("Enter the average temperature: "))
+		if (day < 60)
+			heating = heating + 1
+		else 
+			cooling = cooling + 1
+		choice = eval(input("Is there another day? (y/n)"))
+
+	print("Number of heating days:",heating,"\nNumber of cooling days:",cooling)
+main()
